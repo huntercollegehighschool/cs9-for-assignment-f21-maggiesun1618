697e0d8043aeb2cf44f325808abb02ad100dcd81
Maggie Sun <93930210+maggiesun1618@users.noreply.github.com>
2021-12-19 19:33:04 +0000

    I finished this.

--- part2.py
+++ part2.py
@@ -8,3 +8,10 @@ For example, if the user types in 4, the program should print 100 (since 1^3 + 2
 """
 
 #write your code here
+number = int(input("Enter a positive integer: "))
+sumofcubes = 0
+
+for number in range(1,number + 1):
+  sumofcubes = sumofcubes + number**3
+
+print(sumofcubes)
--- part4.py
+++ part4.py
@@ -40,3 +40,14 @@ fizz
 """
 
 # write your code here 
+number = int(input("Enter a number: "))
+
+for number in range(1,number + 1):
+  if number%15 == 0:
+    print("fizzbuzz")
+  elif number%3 == 0:
+    print("fizz")
+  elif number%5 == 0:
+    print("buzz")
+  else:
+    print(number)
